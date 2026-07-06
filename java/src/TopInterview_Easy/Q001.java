90d27cc05ec7978d247b8ed36ae6435cd01e76ec
oowgnus <sungwoo9027@naver.com>
2026-07-06 17:21:49 +0900

    1480 비기너 1번 문제

---
+++ java/src/TopInterview_Easy/Q001.java
@@ -0,0 +1,13 @@
+package TopInterview_Easy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q001 {
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    
+    int [] nums = new int[]
+  }
+}
